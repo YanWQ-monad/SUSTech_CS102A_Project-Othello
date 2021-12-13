bbac43e35461fb7570d6378e634d5ef5949f83f6
YanWQ-monad <YanWQmonad@gmail.com>
2021-12-13 11:41:06 +0800

    feat(ui): add back button

--- src/main/kotlin/com/monadx/othello/ui/components/board/UniversalBoard.kt
+++ src/main/kotlin/com/monadx/othello/ui/components/board/UniversalBoard.kt
@@ -2,6 +2,7 @@ package com.monadx.othello.ui.components.board
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material3.MaterialTheme
@@ -43,6 +44,16 @@ fun UniversalBoard(controller: GamingController) {
         Spacer(Modifier.height(PaneConfig.PANE_DIVIDE))
 
         Row {
+            TextButton(
+                onClick = { controller.appState.goBackMenu() },
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                ),
+            ) {
+                Text("Back")
+            }
+
             TextButton(
                 onClick = { controller.undo() }
             ) {
